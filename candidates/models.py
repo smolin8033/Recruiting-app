19cbc9f41bdc29119a3d15afa787478de321e3cd
Александр Смолин <smolll123@mail.ru>
2022-05-19 12:57:02 +0300

    models are written

--- candidates/models.py
+++ candidates/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Candidate(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    first_name = models.CharField(max_length=50)
+    second_name = models.CharField(max_length=50)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
+
+
+class Value(models.Model):
+    name = models.CharField(max_length=50)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
