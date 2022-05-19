9d4b9ea4d4c47934dcd9a4ce3594cfbdca54c1ce
Александр Смолин <smolll123@mail.ru>
2022-05-19 21:19:19 +0300

    model forms are written

--- candidates/forms.py
+++ candidates/forms.py
@@ -1,6 +1,30 @@
 from django import forms
 
+from candidates.models import Candidate, Tag, Value
+
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class TagForm(forms.ModelForm):
+    name = forms.CharField(label='Add info tag:', max_length=50)
+    candidate = forms.ModelChoiceField(queryset=Candidate.objects.all())
+
+    class Meta:
+        model = Tag
+        fields = ['name']
+
+
+class ValueForm(forms.ModelForm):
+    name = forms.CharField(label='Add information, related to the tag:', max_length=50)
+    tag = forms.ModelChoiceField(queryset=Tag.objects.all())
+    candidate = forms.ModelChoiceField(queryset=Candidate.objects.all())
+
+    class Meta:
+        model = Value
+        fields = [
+            'name',
+            'tag',
+        ]
