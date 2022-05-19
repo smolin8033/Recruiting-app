7741b56f78c4777b8f110cc87847abf94ca6ab11
Александр Смолин <smolll123@mail.ru>
2022-05-19 14:08:04 +0300

    register Candidate model

--- candidates/admin.py
+++ candidates/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Tag, Value
+from .models import Candidate, Tag, Value
 
+admin.site.register(Candidate)
 admin.site.register(Tag)
 admin.site.register(Value)
