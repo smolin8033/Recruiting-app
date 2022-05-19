292ace4c102a60235fb963983ea0a900070e1b78
Александр Смолин <smolll123@mail.ru>
2022-05-19 14:44:12 +0300

    add CandidateDetailView to path

--- candidates/urls.py
+++ candidates/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('login/', views.login_view, name='login')
+    path('login/', views.login_view, name='login'),
+    path('candidate/<int:pk>/', views.CandidateDetailView.as_view(), name='candidate'),
 ]
