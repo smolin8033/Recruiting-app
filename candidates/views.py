e09cc1629bdffe8823240177a8191ab17b3532a7
Александр Смолин <smolll123@mail.ru>
2022-05-19 13:38:51 +0300

    write temp login_view

--- candidates/views.py
+++ candidates/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd["username"], password=cd["password"])
+            if user is not None:
+                login(request, user)
+                if user.is_superuser:
+                    return redirect('admin:index')
+                else:
+                    pass
+            else:
+                return HttpResponse('Please, enter valid login and password')
+    else:
+        form = LoginForm()
+    return render(request, "login.html", {"form": form})
